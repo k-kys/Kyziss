badf1beda539dc759404515be1c323e8599a6b8c
k-kys <khuong.kiyoshi@gmail.com>
2020-08-02 03:59:13 +0700

    windows10-02082020

--- Back-end/ChuyenDe1/bt2.php
+++ Back-end/ChuyenDe1/bt2.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="Description" content="Enter your description here"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
-<link rel="stylesheet" href="assets/css/style.css">
-<title>Bài tập 2</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Description" content="Enter your description here"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Bài tập 2</title>
 </head>
 <body>
-<div class="container">
-    <h2>Cho một danh sách các số, hãy tính trung bình giá trị, giá trị lớn nhất, giá trị nhỏ nhất các phần tử trong danh sách đó.</h2>
+    <div class="container">
+        <h2>Cho một danh sách các số, hãy tính trung bình giá trị, giá trị lớn nhất, giá trị nhỏ nhất các phần tử trong danh sách đó.</h2>
 
-    <?php
+        <?php
         $list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         $total = 0;
         $first = array_key_first($list);
@@ -50,9 +50,9 @@
         echo "Giá trị trung bình: " . $avg . "<br/>";
         echo "Giá trị lớn nhất: " . $max . "<br/>";
         echo "Giá trị nhỏ nhất: " . $min . "<br/>";
-    ?>
-</div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        ?>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
